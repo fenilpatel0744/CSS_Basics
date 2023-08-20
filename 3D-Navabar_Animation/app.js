1e266571390f3390f5c95970ff8d78321a8c11cc
Fenil Patel <65109281+fenilpatel0744@users.noreply.github.com>
2023-08-20 17:25:07 +0530

    Delete 3D-Navabar_Animation directory

--- 3D-Navabar_Animation/app.js
+++
@@ -1,6 +0,0 @@
-const hamburger_menu = document.querySelector(".hamburger-menu");
-const container = document.querySelector(".flex-container");
-
-hamburger_menu.addEventListener("click", () => {
-  container.classList.toggle("active");
-});
